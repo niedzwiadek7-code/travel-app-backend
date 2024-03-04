82877aec7dd9f70456b6f8b5af5689f11598edbd
Damian Kliber <damiankliber@gmail.com>
2024-03-04 19:58:51 +0000

    hash password in user factory

--- src/db/factories/user.factory.ts
+++ src/db/factories/user.factory.ts
@@ -1,13 +1,16 @@
 import { setSeederFactory } from 'typeorm-extension'
+import * as argon from 'argon2'
 import { UserEntity } from '../../resources'
 
-export default setSeederFactory(UserEntity, (faker) => {
+export default setSeederFactory(UserEntity, async (faker) => {
   const user = new UserEntity()
+
+  const hashPassword = (password: string): Promise<string> => argon.hash(password)
 
   user.firstName = faker.person.firstName()
   user.lastName = faker.person.lastName()
   user.email = faker.internet.email()
-  user.password = faker.internet.password()
+  user.password = await hashPassword(faker.internet.password())
 
   return user
 })
